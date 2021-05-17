11883e0128a87c6e2519977e81198cc1e79f2614
ry <renyi2009@qq.com>
2021-05-17 18:20:29 +0800

    deploy

---
+++ precache-manifest.67d3d68a2c1454321477c89455078ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7090a4f3da164fcf80b",
+    "url": "/tallyBook-website/css/app.010a4bd3.css"
+  },
+  {
+    "revision": "416bae1411f43bd9e438370c131a6c0c",
+    "url": "/tallyBook-website/index.html"
+  },
+  {
+    "revision": "c7090a4f3da164fcf80b",
+    "url": "/tallyBook-website/js/app.2591f3c5.js"
+  },
+  {
+    "revision": "9d520cce81130de2f88f",
+    "url": "/tallyBook-website/js/chunk-vendors.7eb1a7fa.js"
+  },
+  {
+    "revision": "ff270a0a0f67cbb5ab608fa0ac3a2f5e",
+    "url": "/tallyBook-website/manifest.json"
+  },
+  {
+    "revision": "da7b7109eeceb17dc24aabd747cdf897",
+    "url": "/tallyBook-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tallyBook-website/robots.txt"
+  }
+]);
